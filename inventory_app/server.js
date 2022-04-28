d7aa169a5ee2ffdeafb49015616fc79ab627a960
IT20146238 <it20146238@my.sliit.lk>
2022-04-28 20:03:09 +0530

    create style css and view html page  in inventory

--- inventory_app/server.js
+++ inventory_app/server.js
@@ -1,7 +1,10 @@
 // Create HTTP SERVER
 const express = require("express");
 const dotenv = require("dotenv");
-const morgan = require("morgan");
+const morgan = require("morgan"); // add server starting time
+const bodyparser = require("body-parser");
+const path = require("path");
+
 
 const app = express();
 
@@ -12,9 +15,22 @@ const PORT = process.env.PORT || 5000
 // log request
 app.use(morgan('tiny'));
 
+// parser request to body parser
+app.use(bodyparser.urlencoded({ extended: true }))
+
+// set view engine
+app.set("view engine", "ejs");
+// app.set("views",path.resolve(__dirname,"views/ejs"))
+
+// load assets
+app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
+// path -->> css/style.css
+app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
+app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
+
 
 app.get('/', (req, res) => {
-    res.send("Inventory Application");
+    res.render('index');
 })
 
 app.listen(PORT, () => {

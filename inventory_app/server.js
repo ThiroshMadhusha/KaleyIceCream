15552d3949dc57af429e8d98838e36ae01829424
IT20146238 <it20146238@my.sliit.lk>
2022-04-29 11:09:18 +0530

    change server js file and add create inventory form and set route path in inventory

--- inventory_app/server.js
+++ inventory_app/server.js
@@ -1,36 +1,44 @@
-// Create HTTP SERVER
+// Create HTTP server
+
 const express = require("express");
 const dotenv = require("dotenv");
-const morgan = require("morgan"); // add server starting time
+const morgan = require("morgan");
 const bodyparser = require("body-parser");
 const path = require("path");
 
 
+const connectDB = require("./server/database/connection");
+
 const app = express();
 
-dotenv.config({path:'config.env'})
+dotenv.config({ path: 'config.env' })
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 
-// log request
+// log requests
 app.use(morgan('tiny'));
 
-// parser request to body parser
-app.use(bodyparser.urlencoded({ extended: true }))
+// mongodb connection
+connectDB();
+// parse request to body-parser
+app.use(bodyparser.urlencoded({ extended: true }));
 
 // set view engine
 app.set("view engine", "ejs");
 // app.set("views",path.resolve(__dirname,"views/ejs"))
 
-// load assets
-app.use("/css", express.static(path.resolve(__dirname, "assets/css")));
-// path -->> css/style.css
-app.use("/js", express.static(path.resolve(__dirname, "assets/js")));
-app.use("/img", express.static(path.resolve(__dirname, "assets/img")));
 
-// load routers
+// load assets
+app.use('/css', express.static(path.resolve(__dirname, "assets/css")))
+// css/style.css
+app.use('/images', express.static(path.resolve(__dirname, "assets/images")));
+app.use('/js', express.static(path.resolve(__dirname, "assets/js")));
+
+// Load Router (router.js eke path okkoma methanata link karanawa)
 app.use("/", require("./server/routes/router"));
 
-app.listen(PORT, () => {
-  console.log(`Server is Running On http://localhost:${PORT}`);
+
+
+app.listen(3000, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
 });

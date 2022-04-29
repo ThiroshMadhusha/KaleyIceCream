a43911a89008f995c665f26940eacc9a1c3f9f3d
IT20146238 <it20146238@my.sliit.lk>
2022-04-29 10:59:15 +0530

    install node module dependency and create inventory view page

---
+++ inventory_app/passport-config.js
@@ -0,0 +1,29 @@
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+
+function initialize(passport, getUserByEmail, getUserById) {
+  const authenticateUser = async (email, password, done) => {
+    const user = await getUserByEmail(email);
+    if (user == null) {
+      return done(null, false, { message: "No user with that email" });
+    }
+
+    try {
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user);
+      } else {
+        return done(null, false, { message: "Password incorrect" });
+      }
+    } catch (err) {
+      return done(e);
+    }
+  };
+
+  passport.use(new LocalStrategy({ usernameField: "email" }, authenticateUser));
+  passport.serializeUser((user, done) => done(null, user.id));
+  passport.deserializeUser(async (id, done) => {
+    return done(null, await getUserById(id));
+  });
+}
+
+module.exports = initialize;

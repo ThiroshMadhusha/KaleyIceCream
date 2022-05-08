9f1082857ad835c7276e0e6daa850beec0dc2f27
IT20146238 <it20146238@my.sliit.lk>
2022-05-08 05:40:28 +0530

    integrate overall assets js alert message files

---
+++ inventory_app/assets/js/home.js
@@ -0,0 +1,43 @@
+$("#add_sales").submit(function (event) {
+  alert("Sales recode added successfully");
+});
+
+$("#update_sales").submit(function (event) {
+  event.preventDefault();
+
+  var unindexed_array = $(this).serializeArray();
+  var data = {};
+
+  $.map(unindexed_array, function (n, i) {
+    data[n["name"]] = n["value"];
+  });
+
+  var request = {
+    url: `http://localhost:3000/api/sales/${data.id}`,
+    method: "PUT",
+    data: data,
+  };
+
+  $.ajax(request).done(function (response) {
+    alert("Record Updated Successfully!");
+  });
+});
+
+if (window.location.pathname == "/") {
+  $ondelete = $(".table tbody td a.delete");
+  $ondelete.click(function () {
+    var id = $(this).attr("data-id");
+
+    var request = {
+      url: `http://localhost:3000/api/sales/${id}`,
+      method: "DELETE",
+    };
+
+    if (confirm("Do you really want to delete this record?")) {
+      $.ajax(request).done(function (response) {
+        alert("Data Deleted Successfully!");
+        location.reload();
+      });
+    }
+  });
+}

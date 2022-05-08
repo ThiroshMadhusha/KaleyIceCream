f5aecf6aa2ca52613177f13fe95a568982509a9c
IT20146238 <it20146238@my.sliit.lk>
2022-05-08 05:51:57 +0530

    integrate overall routes api path file

--- inventory_app/server/routes/router.js
+++ inventory_app/server/routes/router.js
@@ -4,10 +4,17 @@ const route = express.Router();
 // render.js eken callback function
 const services = require("../services/render");
 
+const salesservices = require("../services/Srender");
+
+const employeeservices = require("../services/Erender");
 
 // add controller routes
 const controller = require("../controller/controller");
-// add order controller routes
+
+const salescontroller = require("../controller/Scontroller");
+
+const employeecontroller = require("../controller/Econtroller");
+
 
 // -----------------------------------------Inventory Form Paths--------------------------------------------
 
@@ -41,5 +48,61 @@ route.post("/api/inventorys", controller.create);
 route.get("/api/inventorys", controller.find);
 route.put("/api/inventorys/:id", controller.update);
 route.delete("/api/inventorys/:id", controller.delete);
+
+
+
+
+
+// ---------------Sales Management----------------------------
+
+route.get("/sales", salesservices.homeRoutesSales);
+
+route.get("/add-sales", salesservices.add_sales);
+
+route.get("/update-sales", salesservices.update_sales);
+
+route.get("/view-sales", salesservices.view_sales);
+
+route.post("/api/sales", salescontroller.create);
+route.get("/api/sales", salescontroller.find);
+route.put("/api/sales/:id", salescontroller.update);
+route.delete("/api/sales/:id", salescontroller.delete);
+
+
+
+// -----------------------------Employee Management-------------------------
+
+
+/**
+ * @description Root Route
+ * @method GET
+ */
+route.get("/", employeeservices.homeRoutesEmployee);
+
+/**
+ * @description add users
+ * @method GET /add-user
+ */
+route.get("/add-user", employeeservices.add_user);
+
+/**
+ * @description update users
+ * @method GET /update-user
+ */
+route.get("/update-user", employeeservices.update_user);
+
+/**
+ * @description user profile
+ * @method GET /user-profile
+ */
+route.get("/user-profile", employeeservices.user_profile);
+
+
+//API
+route.post("/api/userss", employeecontroller.create);
+route.get("/api/userss", employeecontroller.find);
+route.put("/api/userss/:id", employeecontroller.update);
+route.delete("/api/userss/:id", employeecontroller.delete);
+
 
 module.exports = route;
